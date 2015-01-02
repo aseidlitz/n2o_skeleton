f0b0eabb0e356cc5d029ad89ca1036b0ef251887
Alex Popov <alex.popov@ubc.ca>
2015-01-02 13:47:25 -0800

    hardcoded deps/*/ebin folder names to workaround windows shell limitations

--- start.cmd
+++ start.cmd
@@ -1,1 +1,12 @@
-erl -name skel@localhost -pa deps/*/ebin -pa apps/*/ebin -boot start_sasl -s skel_app start
+erl -name skel@localhost ^
+	-pa deps/cowboy/ebin ^
+	-pa deps/cowlib/ebin ^
+	-pa deps/erlydtl/ebin ^
+	-pa deps/gproc/ebin ^
+	-pa deps/n2o/ebin ^
+	-pa deps/ranch/ebin ^
+	-pa apps/skel/ebin ^
+	-boot start_sasl ^
+	-s skel_app start
+
+
